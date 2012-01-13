120c051938217c1981db43cdb0dd669c3818e62a
yhahn <yfhahn@gmail.com>
2012-01-13 17:01:08 -0500

    Add --name flag.

--- cefclient/cefclient_win.cpp
+++ cefclient/cefclient_win.cpp
@@ -20,8 +20,6 @@
 
 // Global Variables:
 HINSTANCE hInst;								// current instance
-TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 char szWorkingDir[MAX_PATH];   // The current working directory
 UINT uFindMsg;  // Message identifier for find events.
 HWND hFindDlg = NULL; // Handle for the find dialog.
@@ -68,16 +66,40 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
 
   HACCEL hAccelTable;
 
-  // Initialize global strings
-  LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-  LoadString(hInstance, IDC_CEFCLIENT, szWindowClass, MAX_LOADSTRING);
-  MyRegisterClass(hInstance);
+  CefString szName = g_command_line->GetSwitchValue("name");
+  LPCWSTR szNamew = szName.c_str();
 
-  // Perform application initialization
-  if (!InitInstance (hInstance, nCmdShow))
-  {
-    return FALSE;
-  }
+  WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CEFCLIENT));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_CEFCLIENT);
+	wcex.lpszClassName	= szNamew;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	if (!RegisterClassEx(&wcex)) return FALSE;
+
+  // Perform window initialization
+  HWND hWnd;
+  hInst = hInstance; // Store instance handle in our global variable
+  hWnd = CreateWindow(
+	  szNamew,
+	  szNamew,
+      WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+	  CW_USEDEFAULT, 0,
+	  800, 600, // @TODO set these from the command line
+	  NULL, NULL, hInstance, NULL);
+
+  if (!hWnd) return FALSE;
+
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
 
   hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CEFCLIENT));
 
@@ -112,73 +134,6 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
   CefShutdown();
 
   return result;
-}
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
-//  COMMENTS:
-//
-//    This function and its usage are only necessary if you want this code
-//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
-//    function that was added to Windows 95. It is important to call this
-//    function so that the application will get 'well formed' small icons
-//    associated with it.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-	WNDCLASSEX wcex;
-
-	wcex.cbSize = sizeof(WNDCLASSEX);
-
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CEFCLIENT));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_CEFCLIENT);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-	return RegisterClassEx(&wcex);
-}
-
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   HWND hWnd;
-
-   hInst = hInstance; // Store instance handle in our global variable
-
-   hWnd = CreateWindow(szWindowClass, szTitle,
-      WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
-	  CW_USEDEFAULT, 0,
-	  800, 600, // @TODO set these from the command line
-	  NULL, NULL, hInstance, NULL);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
 }
 
 //
@@ -231,7 +186,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         CefBrowser::CreateBrowser(info,
             static_cast<CefRefPtr<CefClient> >(g_handler),
 			url, settings);
-            // "http://www.google.com", settings);
       }
       return 0;
 
