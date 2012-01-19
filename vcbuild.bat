6e2a84251cb1bb29333b3c8b79649e6c85db99c0
Dane Springmeyer <dane@dbsgeo.com>
2012-01-18 23:14:57 -0800

    copy exe into tilemill to deploy

--- vcbuild.bat
+++ vcbuild.bat
@@ -1,2 +1,3 @@
 @rem msbuild cefclient.vcxproj /t:clean /p:Configuration=Release /p:Platform=Win32
 msbuild cefclient.vcxproj /p:Configuration=Release /p:Platform=Win32
+copy Release\cefclient.exe ..\tilemill\node_modules\topcube\cefclient\cefclient.exe
